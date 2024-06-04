2324ee9979581870034e03c8dd6b7439dd9d9ab8
ninazmul <nazmulsaw@gmail.com>
2024-06-04 11:46:27 +0600

    why us responsive done

--- src/Components/WhyUs.jsx
+++ src/Components/WhyUs.jsx
@@ -8,10 +8,12 @@ import img6 from "../assets/Fast-icon.png";
 
 export default function WhyUs() {
   return (
-    <div className="m-20 flex justify-around gap-20 h-96 items-center relative">
-      <div className="flex flex-col gap-4 w-1/2">
-        <h1 className="text-6xl font-inter font-bold uppercase">Why Choose Us?</h1>
-        <p className="text-gray-500">
+    <div className="m-10 md:m-20 flex flex-wrap justify-around gap-20 h-[750px] md:h-96 items-center relative">
+      <div className="flex flex-col gap-4 md:w-1/2 justify-center md:justify-start items-center md:items-start">
+        <h1 className="text-4xl text-center md:text-start md:text-6xl font-inter font-bold uppercase">
+          Why Choose Us?
+        </h1>
+        <p className="text-gray-500 text-center md:text-start">
           Our reputation has been built by word-of-mouth referrals since the
           very beginning in 2010, and we’re committed to providing the same
           level of quality service to you.
@@ -24,27 +26,27 @@ export default function WhyUs() {
         </Button>
       </div>
       <div className="flex-1">
-        <div className="bg-[#325B64] rounded-lg flex flex-col items-center justify-center w-56 h-40 p-4 text-center gap-4 absolute bottom-60 right-72">
+        <div className="bg-[#325B64] rounded-lg flex flex-col items-center justify-center w-36 m-10 lg:w-56 h-40 p-4 text-center gap-4 absolute bottom-20 md:bottom-60 right-10 lg:right-72">
           <img src={img6} alt="" className="w-20" />
           <p className="text-gray-500 text-sm">Eco-Friendly Products</p>
         </div>
-        <div className="bg-[#28515A] rounded-lg flex flex-col items-center justify-center w-56 h-40 p-4 text-center gap-4 absolute bottom-48 right-64">
+        <div className="bg-[#28515A] rounded-lg flex flex-col items-center justify-center w-40 m-8 lg:w-56 h-40 p-4 text-center gap-4 absolute bottom-16 md:bottom-48 right-10 lg:right-64">
           <img src={img5} alt="" className="w-20" />
           <p className="text-gray-500 text-sm">Eco-Friendly Products</p>
         </div>
-        <div className="bg-[#1E4750] rounded-lg flex flex-col items-center justify-center w-56 h-40 p-4 text-center gap-4 absolute bottom-36 right-56">
+        <div className="bg-[#1E4750] rounded-lg flex flex-col items-center justify-center w-44 m-6 lg:w-56 h-40 p-4 text-center gap-4 absolute bottom-12 md:bottom-36 right-10 lg:right-56">
           <img src={img4} alt="" className="w-20" />
           <p className="text-gray-500 text-sm">Eco-Friendly Products</p>
         </div>
-        <div className="bg-[#143D46] rounded-lg flex flex-col items-center justify-center w-56 h-40 p-4 text-center gap-4 absolute bottom-24 right-48">
+        <div className="bg-[#143D46] rounded-lg flex flex-col items-center justify-center w-48 m-4 lg:w-56 h-40 p-4 text-center gap-4 absolute bottom-8 md:bottom-24 right-10 lg:right-48">
           <img src={img3} alt="" className="w-20" />
           <p className="text-gray-500 text-sm">Eco-Friendly Products</p>
         </div>
-        <div className="bg-[#14333C] rounded-lg flex flex-col items-center justify-center w-56 h-40 p-4 text-center gap-4 absolute bottom-12 right-40">
+        <div className="bg-[#14333C] rounded-lg flex flex-col items-center justify-center w-52 m-2 lg:w-56 h-40 p-4 text-center gap-4 absolute bottom-4 md:bottom-12 right-10 lg:right-40">
           <img src={img2} alt="" className="w-20" />
           <p className="text-gray-500 text-sm">Eco-Friendly Products</p>
         </div>
-        <div className="bg-[#002932] rounded-lg flex flex-col items-center justify-center w-56 h-40 p-4 text-center gap-4 absolute bottom-0 right-32">
+        <div className="bg-[#002932] rounded-lg flex flex-col items-center justify-center w-56 h-40 p-4 text-center gap-4 absolute bottom-0 right-10 lg:right-32">
           <img src={img1} alt="" className="w-20" />
           <p className="text-gray-500 text-sm">Eco-Friendly Products</p>
         </div>
