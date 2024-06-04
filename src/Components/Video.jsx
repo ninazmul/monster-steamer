ff6f4c67bece727e25f2a362a9d58eb5b53fef7d
ninazmul <nazmulsaw@gmail.com>
2024-06-04 11:50:52 +0600

    video section responsive done

--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -3,13 +3,13 @@ import { MdPlayArrow } from "react-icons/md";
 
 export default function Video() {
   return (
-    <div className="relative flex justify-center items-center m-20">
+    <div className="relative flex justify-center items-center m-10 md:mx-20 md:my-28">
       <img
         src={vid}
         alt="Video"
-        className="h-[550px] w-full object-cover rounded-lg z-10"
+        className="h-[170px] md:h-[350px] lg:h-[550px] w-full object-cover rounded-lg z-10"
       />
-      <div className="absolute bottom-10 right-10 z-20">
+      <div className="absolute bottom-5 md:bottom-10 right-5 md:right-10 z-20">
         <MdPlayArrow className="text-4xl md:text-6xl lg:text-8xl text-white bg-[#00CCF8] rounded-full" />
       </div>
     </div>
